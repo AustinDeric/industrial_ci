013529af07816d31604fefa54af0f026abc19c90
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2018-11-27 13:42:38 -0800

    [feat:commit_img] Simplified test (attempting to address https://github.com/ros-industrial/industrial_ci/pull/319/files#r230591026)
    [feat:commit_img] Simplify test logic (based on suggestion https://github.com/AustinDeric/industrial_ci/pull/3#discussion_r236876930).
    
    [feat:commit_img] Removed obsolete logic (address https://github.com/ros-industrial/industrial_ci/pull/319#discussion_r230591386).

--- industrial_ci/src/docker.sh
+++ industrial_ci/src/docker.sh
@@ -111,7 +111,7 @@ function ici_run_cmd_in_docker() {
   wait %% || ret=$?
   trap - INT
   if [ -n "$commit_image" ]; then
-      docker commit -m "$DOCKER_COMMIT_MSG" "$cid" "$commit_image"
+    docker commit -m "$DOCKER_COMMIT_MSG" "$cid" "$commit_image"
   fi
   docker rm "$cid" > /dev/null
   return $ret
